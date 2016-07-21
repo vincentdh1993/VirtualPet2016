f296520d1cee31d8dab5368d0f8942c20643369b
Vincent Lee <dongho.vincent.lee@gmail.com>
2016-07-21 13:41:22 -0400

    fail

--- pages/Register/client/register.js
+++ pages/Register/client/register.js
@@ -2,6 +2,7 @@ Template.register.onCreated(function() {
     this.state = new ReactiveDict();
     this.state.setDefault({
          lastError:null,
+         counter: 0,
     });
 });
 
@@ -9,7 +10,14 @@ Template.register.helpers({
     errorMessage: function() {
         const instance = Template.instance();
         return instance.state.get("lastError");
-    }
+    },
+    email: function(){
+        return $('div.carousel-inner .item').children("img").get(0).height;
+    },
+    theCounter: function(){
+    const instance = Template.instance();
+    return instance.state.get("counter");
+  },
 })
 
 Template.register.events({
@@ -20,6 +28,7 @@ Template.register.events({
         const nickname = $(".js-nickname").val()
         const petname =  $(".js-petname").val()
         const petshape = $(".carousel-inner").val();
+        calculate();
         
         var user = {
             email: email,
@@ -36,9 +45,24 @@ Template.register.events({
                 Meteor.call("addUserProf",user);
                 instance.state.set("lastError",null);
                 Router.go('home');
-                
             }
         });
-    }
+    },
+    "click .js-right": function(event,instance){
+    const c = instance.state.get("counter");
+    instance.state.set("counter",c+1);
+    },
+    "click .js-left": function(event,instance){
+    const c = instance.state.get("counter");
+    instance.state.set("counter",c-1);
+  },
 });
+
+
+function calculate(){
+    const c = theCounter;
+    if (c/3==1){
+        console.log("3 is ");
+    }
+}
 
